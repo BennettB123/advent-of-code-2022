062b562d4e9f57ff262c55a952f0a4381a482400
Bennett Ballentine <bennett.ballentine@gmail.com>
2023-04-24 16:53:02 -0400

    skeleton for day 11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,46 @@
+use std::collections::VecDeque;
+
+struct Monkey {
+    items: VecDeque<i32>
+}
+
+impl Monkey {
+    fn new(items: VecDeque<i32>) -> Self {
+        Monkey {
+            items,
+        }
+    }
+
+    fn add(&mut self, item: i32) {
+        self.items.push_back(item);
+    }
+
+    fn remove(&mut self) -> Option<i32> {
+        self.items.pop_front()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // initialize all monkeys with their starting items
+
+    // Do 10 rounds
+    for _ in 0..20 {
+        // handle Monkey 0
+
+        // handle Monkey 1
+
+        // handle Monkey 2
+
+        // handle Monkey 3
+
+        // handle Monkey 4
+
+        // handle Monkey 5
+
+        // handle Monkey 6
+
+        // handle Monkey 7
+
+    }
+    
 }
