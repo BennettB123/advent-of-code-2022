a19ed43c789e85100cb278439ca222eeb08e8235
Bennett Ballentine <bennett.ballentine@gmail.com>
2023-04-28 22:24:28 -0400

    day 12 complete!

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,7 +1,14 @@
 // Advent of Code 2022
 // Day 12
+//
+// Notes: This code is pretty sloppy, so read at your own risk.
+//  Part 1 is solved by using a breadth-first search. Starting at the S, check all neighbors, then check the neighbor's neighbors, etc until
+//    the end is found.
+//  My solution to part 2 of this puzzle is extremely unoptimized. I essentially do the same breadth-first search starting from EVERY 'a' on the map.
+//    This was 1338 of them on my map, so this took a while to run. I know a faster approach would be to do the BFS starting from the end-point, but I
+//    already spent enough time on this day's puzzle and wanted to move on...
 
-use std::{collections::VecDeque, env, fs};
+use std::{cmp::min, collections::VecDeque, env, fs};
 
 const START_CHAR: char = 'S';
 const END_CHAR: char = 'E';
@@ -21,16 +28,36 @@ fn main() {
         Err(err) => panic!("Could not open input file {}. Reason: {}", input_file, err),
     };
 
-    let mut height_map = parse_file_to_height_map(file_contents);
-	height_map.build_distance_map();
+    // Get part 1 answer
+    let mut height_map_part_1 = parse_file_to_height_map(&file_contents);
+    height_map_part_1.explore_until_end(height_map_part_1.start_x, height_map_part_1.start_y);
+
+    // Get part 2 answer (super duper slow)
+    let mut height_map_part_2 = parse_file_to_height_map(&file_contents);
+    let mut part_2_shortest = i32::MAX;
+    for y in 0..height_map_part_2.height() {
+        for x in 0..height_map_part_2.width() {
+            if height_map_part_2.elevation_at_x_y(x, y) == 1 {
+                height_map_part_2.explore_until_end(x, y);
+                part_2_shortest = min(part_2_shortest, height_map_part_2.distance_to_end());
+            }
+        }
+    }
 
     println!("################################");
     println!("#### Advent of Code, Day 12 ####");
     println!("################################");
-    println!("quickest path is {} moves", height_map.distance_to_end());
+    println!(
+        "Quickest path from S to E is {} moves",
+        height_map_part_1.distance_to_end()
+    );
+    println!(
+        "Shortest path from any low-point to E is {} moves",
+        part_2_shortest
+    );
 }
 
-fn parse_file_to_height_map(contents: String) -> HeightMap {
+fn parse_file_to_height_map(contents: &String) -> HeightMap {
     let mut map: Vec<Vec<u8>> = vec![];
     let mut start_x = 0;
     let mut start_y = 0;
@@ -88,7 +115,6 @@ struct HeightMap {
     end_x: usize,
     end_y: usize,
     distances: Vec<Vec<i32>>,
-    checked_locations: VecDeque<(usize, usize)>,
 }
 
 impl HeightMap {
@@ -110,7 +136,6 @@ impl HeightMap {
             end_x,
             end_y,
             distances: distances,
-            checked_locations: VecDeque::new(),
         };
         ret.set_distance(start_x, start_y, 0);
         ret
@@ -135,57 +160,37 @@ impl HeightMap {
         self.distances[y][x]
     }
 
-	fn distance_to_end(&self) -> i32 {
-		self.distance_at_x_y(self.end_x, self.end_y)
-	}
+    fn distance_to_end(&self) -> i32 {
+        self.distance_at_x_y(self.end_x, self.end_y)
+    }
 
     fn set_distance(&mut self, x: usize, y: usize, distance: i32) {
         self.distances[y][x] = distance;
     }
 
-    fn find_eligible_neighbors(&mut self, x: usize, y: usize) -> Vec<(usize, usize)> {
-		let mut new_neighbors = vec![];
+    fn find_neighbors(&mut self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        let mut new_neighbors = vec![];
         // up
         if let Some((new_x, new_y)) = self.get_neighbor_up(x, y) {
-            if !self.have_visited(new_x, new_y) && !self.checked_locations.contains(&(new_x, new_y))
-            {
-                self.checked_locations.push_back((new_x, new_y));
-				new_neighbors.push((new_x, new_y));
-            }
+            new_neighbors.push((new_x, new_y));
         }
 
         // down
         if let Some((new_x, new_y)) = self.get_neighbor_down(x, y) {
-            if !self.have_visited(new_x, new_y) && !self.checked_locations.contains(&(new_x, new_y))
-            {
-                self.checked_locations.push_back((new_x, new_y));
-				new_neighbors.push((new_x, new_y));
-            }
+            new_neighbors.push((new_x, new_y));
         }
 
         // left
         if let Some((new_x, new_y)) = self.get_neighbor_left(x, y) {
-            if !self.have_visited(new_x, new_y) && !self.checked_locations.contains(&(new_x, new_y))
-            {
-                self.checked_locations.push_back((new_x, new_y));
-				new_neighbors.push((new_x, new_y));
-            }
+            new_neighbors.push((new_x, new_y));
         }
 
         // right
         if let Some((new_x, new_y)) = self.get_neighbor_right(x, y) {
-            if !self.have_visited(new_x, new_y) && !self.checked_locations.contains(&(new_x, new_y))
-            {
-                self.checked_locations.push_back((new_x, new_y));
-				new_neighbors.push((new_x, new_y));
-            }
+            new_neighbors.push((new_x, new_y));
         }
 
-		new_neighbors
-    }
-
-    fn have_visited(&self, x: usize, y: usize) -> bool {
-        self.distance_at_x_y(x, y) >= 0
+        new_neighbors
     }
 
     fn get_neighbor_up(&self, x: usize, y: usize) -> Option<(usize, usize)> {
@@ -228,18 +233,38 @@ impl HeightMap {
         }
     }
 
-    fn build_distance_map(&mut self) {
-        self.explore(self.start_x, self.start_y, 0);
-    }
+    fn explore_until_end(&mut self, starting_x: usize, starting_y: usize) {
+        // queue_to_check holds ((x, y), prev_neighbor_distance)
+        let mut queue_to_check = VecDeque::<((usize, usize), i32)>::new();
+        let mut explored = Vec::<(usize, usize)>::new();
 
-	// TODO: THIS IS DEPTH FIRST SEARCH, NOT BREADTH FIRST!!!!
-    fn explore(&mut self, x: usize, y: usize, curr_dist: i32) {
-		println!("Exploring ({},{}). Distance {}", x, y, curr_dist);
-		self.set_distance(x, y, curr_dist);
-		
-        for n in self.find_eligible_neighbors(x, y) {
-			self.explore(n.0, n.1, curr_dist + 1);
-		}
+        // handle starting location
+        self.set_distance(starting_x, starting_y, 0);
+        explored.push((starting_x, starting_y));
+        for n in self.find_neighbors(starting_x, starting_y) {
+            queue_to_check.push_back((n, 0));
+            explored.push(n);
+        }
+
+        while queue_to_check.len() > 0 {
+            let curr = queue_to_check.pop_front().unwrap();
+            let curr_x = curr.0 .0;
+            let curr_y = curr.0 .1;
+            let last_dist = curr.1;
+            self.set_distance(curr_x, curr_y, last_dist + 1);
+
+            // end early if we reached the end
+            if curr_x == self.end_x && curr_y == self.end_y {
+                break;
+            }
+
+            for n in self.find_neighbors(curr_x, curr_y) {
+                if !explored.contains(&n) {
+                    queue_to_check.push_back((n, last_dist + 1));
+                    explored.push(n);
+                }
+            }
+        }
     }
 
     fn height_map_to_string(&self) -> String {
@@ -263,13 +288,13 @@ impl HeightMap {
         let mut s = String::new();
         for y in 0..self.height() {
             for x in 0..self.width() {
-                if x == self.start_x && y == self.start_y {
-                    s += &format!("{: >5}", START_CHAR);
-                } else if x == self.end_x && y == self.end_y {
-                    s += &format!("{: >5}", END_CHAR);
-                } else {
-                    s += &format!("{: >5}", &self.distance_at_x_y(x, y))
-                }
+                // if x == self.start_x && y == self.start_y {
+                //     s += &format!("{: >5}", START_CHAR);
+                // } else if x == self.end_x && y == self.end_y {
+                //     s += &format!("{: >5}", END_CHAR);
+                // } else {
+                s += &format!("{: >5}", &self.distance_at_x_y(x, y))
+                // }
             }
             s += "\n";
         }
