016c0fbb369c15df2347bb2fb8cda2fcc0ae610e
Bennett Ballentine <bennett.ballentine@gmail.com>
2023-05-05 19:41:28 -0400

    day 15: parsing input done

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,7 +1,8 @@
 // Advent of Code 2022
 // Day 15
 
-use std::{env, fs};
+use regex::Regex;
+use std::{env, fmt::Display, fs};
 
 fn main() {
     // get file path from commandline input
@@ -16,8 +17,62 @@ fn main() {
         Err(err) => panic!("Could not open input file {}. Reason: {}", input_file, err),
     };
 
+    let sensors = parse_input(&file_contents);
+
+    for s in sensors {
+        println!("{}", s);
+    }
+
     // print answers
     println!("################################");
     println!("#### Advent of Code, Day 15 ####");
     println!("################################");
+}
+
+fn parse_input(contents: &String) -> Vec<Sensor> {
+    let mut sensors: Vec<Sensor> = vec![];
+
+    let re = Regex::new(
+        r"(?i)^sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$",
+    )
+    .unwrap();
+
+    for line in contents.lines() {
+        if line.trim().is_empty() {
+            continue;
+        }
+
+        let captures = re.captures(line).unwrap();
+        sensors.push(Sensor {
+            x: captures[1].parse().unwrap(),
+            y: captures[2].parse().unwrap(),
+            closest_beacon: Beacon {
+                x: captures[3].parse().unwrap(),
+                y: captures[4].parse().unwrap(),
+            },
+        });
+    }
+
+    sensors
+}
+
+struct Sensor {
+    x: i32,
+    y: i32,
+    closest_beacon: Beacon,
+}
+
+impl Display for Sensor {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Sensor at ({},{}). Closet Beacon at ({},{})",
+            self.x, self.y, self.closest_beacon.x, self.closest_beacon.y
+        )
+    }
+}
+
+struct Beacon {
+    x: i32,
+    y: i32,
 }
